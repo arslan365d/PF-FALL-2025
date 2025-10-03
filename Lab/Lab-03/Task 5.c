c5e36cf33111e007fcba09eb057f75458aa97b21
Arslan Ahmed <142019912+arslan365d@users.noreply.github.com>
2025-10-03 23:51:46 +0500

    Uploaded Task 3,4 and 5

---
+++ Lab/Lab-03/Task 5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    float principal, rate, time, simpleInterest;
+    
+    printf("Simple Interest Calculator!!!");
+    printf("\nRestrictions:\nThe input value of the Principle must be between 100 Rs to 1,000,000 Rs.\nThe Rate of interest must be between 5 to 10 percent\nThe Time period must be between 1 to 10 years.\n");
+
+    // Input
+    printf("Enter Principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter Rate of interest (in %%): ");
+    scanf("%f", &rate);
+
+    printf("Enter Time (in years): ");
+    scanf("%f", &time);
+
+    // Calculate Simple Interest
+    simpleInterest = (principal * rate * time) / 100;
+
+    // Output
+    printf("Simple Interest = %.2f\n", simpleInterest);
+
+    return 0;
+}
