6b33d6044aea4ae7587bf9217ec2d58dbd1cf1aa
Arslan Ahmed <142019912+arslan365d@users.noreply.github.com>
2025-10-22 17:25:29 +0500

    task 3,4 and 5 uploaded of lab7

---
+++ Lab/Lab-07/Task 3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <string.h>
+int main()
+{
+	char firstName[20] , lastName[20],fullName[40];
+	int countOfCharac = 0;
+	printf("Enter your First Name:");
+	scanf("%s",&firstName);
+	printf("Enter your Last Name:");
+	scanf("%s",&lastName);
+	strcat(fullName,firstName);
+	strcat(fullName," ");
+	strcat(fullName,lastName);
+	countOfCharac = strlen(fullName) - 1;
+	printf("Your Full Name is %s",fullName);
+	
+	return 0;
+}
