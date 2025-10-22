adf9f7ae5f7f8502f8c52f9f4b6379b72431ea6a
Arslan Ahmed <142019912+arslan365d@users.noreply.github.com>
2025-10-22 17:40:29 +0500

    edit the file

--- Lab/Lab-07/Task 3.c
+++ Lab/Lab-07/Task 3.c
@@ -11,7 +11,7 @@ int main()
 	strcat(fullName,firstName);
 	strcat(fullName," ");
 	strcat(fullName,lastName);
-	countOfCharac = strlen(fullName) - 1;
+	countOfCharac = strlen(fullName);
 	printf("Your Full Name is %s",fullName);
 	
 	return 0;
