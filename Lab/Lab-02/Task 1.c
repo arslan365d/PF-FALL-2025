0695cc78ba53bee49125c24c428c3e03bfd90cf4
Arslan Ahmed <142019912+arslan365d@users.noreply.github.com>
2025-10-25 00:55:34 +0500

    Lab 2 Examples Uploaded

---
+++ Lab/Lab-02/Task 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+int age;
+printf("Enter your age:");
+scanf("%d",&age);
+if(age>=18){ 
+    printf("You are eligible for Voting");
+}
+else{
+	printf("You are not eligible for Voting");
+}
+
+}
