e5ae03dbca4257584403027247c196efc272b820
Arslan Ahmed <142019912+arslan365d@users.noreply.github.com>
2025-10-25 02:46:04 +0500

    Update Task 6.c

--- Lab/Lab-06/Task 6.c
+++ Lab/Lab-06/Task 6.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main( ){
-int i=2;
+int i=4;
 float sum = 1.00;
 
 while (i <=400){
- sum += 1.0/i;
- i++;
+	if(i % 4 == 0){
+		sum += 1.0/i;
+	}
+  i++;
 }
-printf("The Sum from 1 to 1/400 is %.3f", sum);
+printf("The sum of series 1 + 1/4 + 1/8 + ... + 1/400 is %.3f", sum);
 return 0;
 }
